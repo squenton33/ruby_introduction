ebae16855808e5e4d6e628fc9c3edcf7b7abcfa9
squenton33 <slightlyoriginal@gmail.com>
2023-07-31 14:39:05 -0700

    3 and 4

---
+++ basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { :oppenheimer => 2023,
+           :ironman => 2008,
+           :anchorman => 2004,
+           :christopher_robin => 2018,
+           :into_the_spiderverse => 2018 }
+
+puts movies[:oppenheimer]
+puts movies[:ironman]
+puts movies[:anchorman]
+puts movies[:christopher_robin]
+puts movies[:into_the_spiderverse]
---
+++ basics/exercise_4.rb
@@ -0,0 +1,7 @@
+dates = [2023, 2008, 2004, 2018, 2018]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
