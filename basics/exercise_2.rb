ff8f521e8a5ccff695af761e454634ebe8e26f7c
squenton33 <slightlyoriginal@gmail.com>
2023-07-31 14:28:07 -0700

    exercise 2 attempt

---
+++ basics/exercise_2.rb
@@ -0,0 +1,10 @@
+thousands = 1174 / 1000
+hundreds = 1174 % 1000 / 100
+tens = 1174 % 100 / 10
+ones = 1174 % 10
+
+puts 1174
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
