efcd17efbeef002d4e356f1536796a13bbfccd8a
squenton33 <slightlyoriginal@gmail.com>
2023-07-31 22:23:38 -0700

    exercuse 2

---
+++ hashes/ex_2.rb
@@ -0,0 +1,12 @@
+sibling_name = {name: "Jim"}
+sibling_age = {age: "13"}
+
+puts sibling_name.merge(sibling_age)
+
+puts sibling_name
+puts sibling_age
+
+puts sibling_name.merge!(sibling_age)
+puts sibling_name
+puts sibling_age
+
