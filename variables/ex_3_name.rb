ce434cca82441930202de4fd7c513475b0cfd312
squenton33 <slightlyoriginal@gmail.com>
2023-07-31 18:50:31 -0700

    3 and 4

---
+++ variables/ex_3_name.rb
@@ -0,0 +1,10 @@
+print "What is your name?"
+name = gets.chomp   
+
+puts "Hello there #{name} how are you today?"
+
+puts "Here is your name ten times, you are welcome"
+
+10.times do
+  puts name
+end
---
+++ variables/ex_4_name.rb
@@ -0,0 +1,10 @@
+print "What is your first name?"
+
+first_name = gets.chomp
+
+print "Dope, now what is your last name?"
+
+last_name = gets.chomp
+
+puts "Oh so your full name is something like:"
+puts first_name + ' ' + last_name
