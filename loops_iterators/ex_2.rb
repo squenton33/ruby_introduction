e256762eaaf3943aaa1a26a3598582136e9303d0
squenton33 <slightlyoriginal@gmail.com>
2023-07-31 21:35:24 -0700

    exercise 2

---
+++ loops_iterators/ex_2.rb
@@ -0,0 +1,12 @@
+x = ""
+while x != "Stop" do
+  puts "This is the song that never ends"
+  ans = gets.chomp 
+  puts "it just goes on and on my friends"
+  ans = gets.chomp 
+  puts "I just started singing it not knowing what it was"
+  ans = gets.chomp 
+  puts "and I'll continue singing it forever just because"
+  x = gets.chomp 
+end
+
